6e656ae03fb1f071c473e60d0255b50d8aa00491
SolidHinken <a.arutyunyan@gamedelta.io>
2020-06-28 16:36:30 +0300

    Initial commit

---
+++ Assets/Internal Assets/_Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed = 1f;  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
+        {
+            transform.Translate(Input.GetAxis("Horizontal") * speed, 0f, Input.GetAxis("Vertical") * speed);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PlanetSim
+ 
